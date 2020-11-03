5caa7acfaf2c34e147f557a6eb60db953aa35c67
vuthihong231020 <vuthihong23102000@gmail.com>
2020-11-03 15:34:53 +0700

    Add project files.

---
+++ demo_git_github/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_git_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xin chào các bạn");
+            Console.WriteLine("Hôm nay lớp 101186 đang thực hành môn đảm bảo và chất lượng phần mềm");
+            Console.WriteLine("Giáo viên giảng dạy: cô Ngô Thanh Huyền hihi");
+            Console.ReadKey();
+        }
+    }
+}
